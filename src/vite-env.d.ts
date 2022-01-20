f7a50494a8df91c7cf61a874e7646c1e148e7c71
Ja-boop <agusmisiones@gmail.com>
2022-01-20 11:20:21 -0300

    Add env file types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_TESTNET: string;
+    readonly VITE_TEST_PRIVATEKEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
