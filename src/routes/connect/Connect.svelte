f33a0715530f9a3ac3f4cee6fb1175846d38a54d
Ja-boop <agusmisiones@gmail.com>
2022-01-25 10:55:42 -0300

    Fix connect component issues

--- src/routes/connect/Connect.svelte
+++ src/routes/connect/Connect.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
-    import { isPrivateKeyVisible, setInputValue, setConnectionError } from './connectStore';
-    import { getStoredKey, getStellarKeypair, storeKey } from './connectHelpers';
+    import { isPrivateKeyVisible, inputValue, connectionError } from './connectStore';
+    import { encryptPrivateKey, getStellarKeypair, decryptPrivatePair } from './connectHelpers';
     import { publicKey } from '../../store/store';
     import InvalidPrivateKeyError from './errors/InvalidPrivateKeyError';
     import StorageKeyNotFoundError from './errors/StorageKeyNotFoundError';
 
+    async function connectWithSecretKey(key: string): Promise<void> {
+        try {
+            const stellarKeyPair = await getStellarKeypair(key);
+            encryptPrivateKey(key);
+            $publicKey = stellarKeyPair.publicKey();
+        } catch (e) {
+            if (e instanceof InvalidPrivateKeyError) {
+                $publicKey = '';
+                $connectionError = 'Invalid key, please try again';
+            }
+        }
+    }
+
     (async function connectWithStorage(): Promise<void> {
         try {
-            const privateKey = await getStoredKey();
+            const privateKey = await decryptPrivatePair();
             connectWithSecretKey(privateKey);
         } catch (e) {
             if (e instanceof StorageKeyNotFoundError) {
@@ -15,19 +28,6 @@
             }
         }
     })();
-
-    async function connectWithSecretKey(key: string): Promise<void> {
-        try {
-            const stellarKeyPair = await getStellarKeypair(key);
-            storeKey(key);
-            $publicKey = stellarKeyPair.publicKey();
-        } catch (e) {
-            if (e instanceof InvalidPrivateKeyError) {
-                $publicKey = '';
-                $setConnectionError = 'Invalid key, please try again';
-            }
-        }
-    }
 </script>
 
 <h1>Connector</h1>
@@ -40,20 +40,20 @@
 {#if $publicKey}
     <h1 id="title">Public Key: {$publicKey}</h1>
 {:else}
-    <h1 id="title">Public Key: {$setConnectionError ? $setConnectionError : 'waiting connection...'}</h1>
+    <h1 id="title">Public Key: {$connectionError ? $connectionError : 'waiting connection...'}</h1>
 {/if}
 
 <button on:click="{() => ($isPrivateKeyVisible = !$isPrivateKeyVisible)}">Show key</button>
 
 {#if $isPrivateKeyVisible}
-    <input id="input-key" type="text" bind:value="{$setInputValue}" />
+    <input id="input-key" type="text" bind:value="{$inputValue}" />
 {:else}
-    <input id="input-key" type="password" bind:value="{$setInputValue}" />
+    <input id="input-key" type="password" bind:value="{$inputValue}" />
 {/if}
 
 <button
     on:click="{() => {
-        connectWithSecretKey($setInputValue);
+        connectWithSecretKey($inputValue);
     }}"
 >
     Connect with private key
