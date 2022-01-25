ae968bbf0e9e04d5a87b58e9f5d7e1440930051b
Ja-boop <agusmisiones@gmail.com>
2022-01-25 10:53:25 -0300

    connect helper works with key manager

--- src/routes/connect/connectHelpers.ts
+++ src/routes/connect/connectHelpers.ts
@@ -1,11 +1,7 @@
 import { getEncryptedData, decryptPrivateKey } from '../../helpers/security';
-import { storeData, getStoredData } from '../../helpers/storage';
 import { Keypair } from 'stellar-sdk';
 import InvalidPrivateKeyError from './errors/InvalidPrivateKeyError';
-import StorageKeyNotFoundError from './errors/StorageKeyNotFoundError';
-
-const CRYPTO_KEY_ITEM_NAME = 'cryptoKey';
-const PRIVATE_KEY_ITEM_NAME = 'privateKey';
+import { storePair, getStoredPair } from '../../helpers/keyManager';
 
 export async function getStellarKeypair(key: string): Promise<Keypair> {
     try {
@@ -16,20 +12,13 @@ export async function getStellarKeypair(key: string): Promise<Keypair> {
     }
 }
 
-export async function storeKey(key: string): Promise<void> {
-    const encryptedPair = await getEncryptedData(key);
-    storeData(CRYPTO_KEY_ITEM_NAME, encryptedPair.cryptoKey);
-    storeData(PRIVATE_KEY_ITEM_NAME, encryptedPair.privateKey);
+export async function encryptPrivateKey(key: string): Promise<void> {
+    const encryptedData = await getEncryptedData(key);
+    storePair(encryptedData.privateKey, encryptedData.cryptoKey);
 }
 
-export async function getStoredKey(): Promise<string> {
-    try {
-        const storedPrivateKey = getStoredData(PRIVATE_KEY_ITEM_NAME);
-        const storedCryptoKey = getStoredData(CRYPTO_KEY_ITEM_NAME);
-
-        const privateKey = await decryptPrivateKey(storedPrivateKey!, storedCryptoKey!);
-        return privateKey;
-    } catch (e) {
-        throw new StorageKeyNotFoundError();
-    }
+export async function decryptPrivatePair(): Promise<string> {
+    const storedPair = getStoredPair();
+    const privateKey = await decryptPrivateKey(storedPair.privateKey, storedPair.cryptoKey);
+    return privateKey;
 }
