d0eac1288f7e33a4ad402c5719d181be3c1104cc
Ja-boop <agusmisiones@gmail.com>
2022-01-25 09:54:51 -0300

    Connect functionalities in component

--- src/routes/connect/Connect.svelte
+++ src/routes/connect/Connect.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
-    import { isPrivateKeyVisible, inputValue, connectionError } from './connectStores';
-    import { setPrivateKey } from './connectHelpers';
-    import { decryptPrivateKey } from '../../helpers/security';
+    import { isPrivateKeyVisible, setInputValue, setConnectionError } from './connectStore';
+    import { getStoredKey, getStellarKeypair, storeKey } from './connectHelpers';
     import { publicKey } from '../../stores/store';
-    import { getStorageData } from '../../helpers/storage';
+    import InvalidPrivateKeyError from './errors/InvalidPrivateKeyError';
     import StorageKeyNotFoundError from './errors/StorageKeyNotFoundError';
 
-    try {
-        const storageKeys = getStorageData();
-        (async () => {
-            const decryptedKey = await decryptPrivateKey(storageKeys.privateKey, storageKeys.cryptoKey);
-            setPrivateKey(decryptedKey);
-        })();
-    } catch (e) {
-        if (e instanceof StorageKeyNotFoundError) {
-            console.log('Key was not found in Storage');
+    (async function connectWithStorage(): Promise<void> {
+        try {
+            const privateKey = await getStoredKey();
+            connectWithSecretKey(privateKey);
+        } catch (e) {
+            if (e instanceof StorageKeyNotFoundError) {
+                console.log('No key was found in storage');
+            }
+        }
+    })();
+
+    async function connectWithSecretKey(key: string): Promise<void> {
+        try {
+            const stellarKeyPair = await getStellarKeypair(key);
+            storeKey(key);
+            $publicKey = stellarKeyPair.publicKey();
+        } catch (e) {
+            if (e instanceof InvalidPrivateKeyError) {
+                $setConnectionError = 'Invalid key, please try again';
+            }
         }
     }
 </script>
@@ -25,17 +35,25 @@
     <li class="simple-signer xbull-wallet">Connect with xBull</li>
     <li class="simple-signer private-key-wallet">Connect with Private Key</li>
 </ul>
-<h1>Public Key: {$publicKey ? $publicKey : $connectionError}</h1>
+
+{#if $publicKey}
+    <h1>Public Key: {$publicKey}</h1>
+{:else}
+    <h1>Public Key: {$setConnectionError ? $setConnectionError : 'waiting connection...'}</h1>
+{/if}
+
 <button on:click="{() => ($isPrivateKeyVisible = !$isPrivateKeyVisible)}">Show key</button>
 
 {#if $isPrivateKeyVisible}
-    <input type="text" bind:value="{$inputValue}" />
+    <input type="text" bind:value="{$setInputValue}" />
 {:else}
-    <input type="password" bind:value="{$inputValue}" />
+    <input type="password" bind:value="{$setInputValue}" />
 {/if}
 
 <button
     on:click="{() => {
-        setPrivateKey($inputValue);
-    }}">Connect with private key</button
+        connectWithSecretKey($setInputValue);
+    }}"
 >
+    Connect with private key
+</button>
