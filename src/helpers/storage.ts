d75b9f5a10277f9d8932e69922609546d41b6812
Ja-boop <agusmisiones@gmail.com>
2022-01-25 09:51:52 -0300

    Storage work as intended

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,18 +1,8 @@
-import type { IStoredKeys } from '../routes/connect/IStoredKeys';
-import StorageKeyNotFoundError from '../routes/connect/errors/StorageKeyNotFoundError';
-
-export function storageData(key: string, cryptoKey: string): void {
-    sessionStorage.setItem('cryptoKey', cryptoKey);
-    sessionStorage.setItem('privateKey', key);
+export function storeData(key: string, value: string): void {
+    sessionStorage.setItem(key, value);
 }
 
-export function getStorageData(): IStoredKeys {
-    const secretKey = sessionStorage.getItem('privateKey');
-    const cryptoKey = sessionStorage.getItem('cryptoKey');
-
-    if (secretKey && cryptoKey) {
-        return { privateKey: secretKey, cryptoKey: cryptoKey };
-    } else {
-        throw new StorageKeyNotFoundError();
-    }
+export function getStoredData(key: string): string | null {
+    const data = sessionStorage.getItem(key);
+    return data;
 }
