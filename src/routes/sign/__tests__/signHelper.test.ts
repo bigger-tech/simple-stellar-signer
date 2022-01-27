c3e288f522f1dc604e253d3200a7ca129c6c21f1
Genebson <mauryavero@gmail.com>
2022-01-27 19:18:15 -0300

    Fix test title

--- src/routes/sign/__tests__/signHelper.test.ts
+++ src/routes/sign/__tests__/signHelper.test.ts
@@ -3,7 +3,7 @@ import { Transaction, Keypair } from 'stellar-sdk';
 import { MOCK_NOT_SIGNED_TRANSACTION } from '../__mocks__/transaction';
 import { expect } from '@jest/globals';
 
-it('Sign transaction with Stellar', () => {
+it('Signs a transaction with a valid key pair', () => {
     const PRIVATE_KEY = 'SA5X2XYC2E4G6VP2CKOB7RDGXEHJ6WA5TSVHGEHWNEKKXH7RZLMUOLWL';
     const sourceKeys = Keypair.fromSecret(PRIVATE_KEY);
     const tx = new Transaction(MOCK_NOT_SIGNED_TRANSACTION, 'Test SDF Network ; September 2015');
