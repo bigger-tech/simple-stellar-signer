dc925a49d5f4377a79af16a9f9713acda159ecd2
Ja-boop <agusmisiones@gmail.com>
2022-01-28 12:06:53 -0300

    delete files from routes

--- src/routes/transaction/operations/CreateAccount.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { Link } from 'svelte-navigator';
-</script>
-
-<Link to="/operation">Go back to operations list</Link>
-
-<h1>Create Account</h1>
-
-<p>Creates and funds a new account with the specified starting balance.</p>
--- src/routes/transaction/operations/Payment.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { Link } from 'svelte-navigator';
-</script>
-
-<Link to="/operation">Go back to operations list</Link>
-
-<h1>Payment</h1>
-
-<p>Sends an amount in a specific asset to a destination account.</p>
