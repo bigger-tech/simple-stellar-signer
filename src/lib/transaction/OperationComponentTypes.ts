4c5216777a7460fb437b224cc1dc85ea92ecc0a5
Ja-boop <agusmisiones@gmail.com>
2022-02-02 17:53:10 -0300

    Variable with all operation component types

---
+++ src/lib/transaction/OperationComponentTypes.ts
@@ -0,0 +1,4 @@
+import type { PaymentComponentType } from './operations/payment/Payment';
+import type { CreateAccountComponentType } from './operations/createAccount/CreateAccount';
+
+export let OperationComponentTypes: PaymentComponentType | CreateAccountComponentType;
