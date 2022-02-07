03055c091806898aae96f800ea5cd3835f5eb268
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-02-07 17:27:05 -0300

    Improve operation classes

--- src/lib/transaction/operations/createAccount/CreateAccount.ts
+++ src/lib/transaction/operations/createAccount/CreateAccount.ts
@@ -1,27 +1,24 @@
-import CreateAccountComponent from './CreateAccount.svelte';
+import CreateAccountComponentSvelte from './CreateAccount.svelte';
+import type IOperationComponent from '../IOperationComponent';
 import type { Operation, Transaction } from 'stellar-sdk';
 import type { SvelteComponent } from 'svelte';
 
-export type CreateAccountComponentType = {
-    component: typeof SvelteComponent;
-    props: {
+export default class CreateAccountComponent implements IOperationComponent {
+    public component: typeof SvelteComponent;
+    public props: {
         optionalSource: string | undefined;
         defaultSource: string;
         destination: string;
         startingBalance: string;
     };
-};
 
-export default class CreateAccount {
-    createOperation(operation: Operation.CreateAccount, tx: Transaction): CreateAccountComponentType {
-        return {
-            component: CreateAccountComponent,
-            props: {
-                optionalSource: operation.source,
-                defaultSource: tx.source,
-                destination: operation.destination,
-                startingBalance: operation.startingBalance,
-            },
+    constructor(tx: Transaction, operation: Operation.CreateAccount) {
+        this.component = CreateAccountComponentSvelte;
+        this.props = {
+            optionalSource: operation.source,
+            defaultSource: tx.source,
+            destination: operation.destination,
+            startingBalance: operation.startingBalance,
         };
     }
 }
