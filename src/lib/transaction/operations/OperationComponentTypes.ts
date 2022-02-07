6ab57fa435791f626df764d482ca2688b081cdfa
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-02-07 17:24:27 -0300

    Add files to the operation folder

---
+++ src/lib/transaction/operations/OperationComponentTypes.ts
@@ -0,0 +1,4 @@
+import type PaymentComponent from './payment/Payment';
+import type CreateAccountComponent from './createAccount/CreateAccount';
+
+export let OperationComponentTypes: PaymentComponent | CreateAccountComponent;
