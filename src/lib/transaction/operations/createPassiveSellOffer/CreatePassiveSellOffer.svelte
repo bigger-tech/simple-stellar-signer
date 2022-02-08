026d2703816c249c4dfb156be8dd340f8536ff13
RivarolaNicolas <RivarolaNicolas94@gmail.com>
2022-02-08 16:53:59 -0300

    Add "Create Pasive Sell Offer" operation

---
+++ src/lib/transaction/operations/createPassiveSellOffer/CreatePassiveSellOffer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let optionalSource: string | undefined;
+    export let defaultSource: string;
+    export let selling: string;
+    export let buying: string;
+    export let amount: string;
+    export let price: string;
+</script>
+
+<div class="simple-signer create-passive-sell-offer-operation">
+    <h3>Operation: Create Passive Sell Offer</h3>
+
+    <p>Source Account: {optionalSource ? optionalSource : defaultSource}</p>
+    <p>Selling: {selling}</p>
+    <p>Buying: {buying}</p>
+    <p>Amount: {amount}</p>
+    <p>Price: {price}</p>
+</div>
