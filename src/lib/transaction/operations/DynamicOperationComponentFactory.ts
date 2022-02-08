7d1531e982b7a582ebfd4d5b2dc5626f12d916b2
RivarolaNicolas <RivarolaNicolas94@gmail.com>
2022-02-07 22:10:10 -0300

    Add "Begin Sponsoring Future Reserves" operation

--- src/lib/transaction/operations/DynamicOperationComponentFactory.ts
+++ src/lib/transaction/operations/DynamicOperationComponentFactory.ts
@@ -3,6 +3,7 @@ import PaymentComponent from './payment/Payment';
 import CreateAccountComponent from './createAccount/CreateAccount';
 import InvalidComponentTypeError from '../../errors/InvalidComponentTypeError';
 import type { OperationComponentTypes } from './OperationComponentTypes';
+import BeginSponsoringFutureReservesComponent from './beginSponsoringFutureReserves/BeginSponsoringFutureReserves';
 
 export default class DynamicOperationComponentFactory {
     create(tx: Transaction, operation: Operation): typeof OperationComponentTypes {
@@ -14,6 +15,9 @@ export default class DynamicOperationComponentFactory {
                 break;
             case 'createAccount':
                 operationComponent = new CreateAccountComponent(tx, operation);
+                break;
+            case 'beginSponsoringFutureReserves':
+                operationComponent = new BeginSponsoringFutureReservesComponent(tx, operation);
                 break;
             default:
                 undefined;
---
+++ src/lib/transaction/operations/beginSponsoringFutureReserves/BeginSponsoringFutureReserves.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let defaultSource: string;
+    export let optionalSource: string | undefined;
+    export let sponsoredId: string;
+</script>
+
+<div class="simple-signer begin-sponsoring-future-reserves-operation">
+    <h3>Operation type: Begin Sponsoring Future Reserves</h3>
+    <p>Source Account: {optionalSource ? optionalSource : defaultSource}</p>
+    <p>Sponsored ID: {sponsoredId}</p>
+</div>
