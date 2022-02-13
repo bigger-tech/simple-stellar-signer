1b67eb087b7d76dcd8a8fdcc22e495582ea1da89
Genebson <mauryavero@gmail.com>
2022-02-12 23:58:24 -0300

    Add new store

--- src/routes/connect/connectStore.ts
+++ src/routes/connect/connectStore.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const isPrivateKeyVisible = writable(false);
+export const isWalletHidden = writable(false);
 export const inputValue = writable('');
 export const connectionError = writable('');
