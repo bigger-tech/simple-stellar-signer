9b6ecb8e8f73d7beffc8da86e694a30b72dd1af0
Genebson <mauryavero@gmail.com>
2022-02-12 23:55:11 -0300

    Fix test

--- cypress/integration/ui/connect.test.ts
+++ cypress/integration/ui/connect.test.ts
@@ -1,9 +1,7 @@
 /// <reference types="cypress" />
 
-describe('connect', () => {
+describe('Connect', () => {
     const url = Cypress.env('HOST');
-    const TEST_PRIVATE_KEY = Cypress.env('TEST_PRIVATE_KEY');
-    const TEST_PUBLIC_KEY = Cypress.env('TEST_PUBLIC_KEY');
 
     beforeEach(() => {
         cy.visit(`${url}connect`);
@@ -14,27 +12,25 @@ describe('connect', () => {
         cy.get('.connect-private-key').as('privateKeyBtn');
         cy.get('.simple-signer-container').as('container');
     });
-    describe('Connect', () => {
-        it("Should check if there's four connect methods", () => {
-            cy.get('@xBullTitle').should('contain.text', 'xBull');
-            cy.get('@freighterTitle').should('contain.text', 'Freighter');
-            cy.get('@albedoTitle').should('contain.text', 'Albedo');
-            cy.get('@privateKeyTitle').should('contain.text', 'Private Key');
-        });
+    it("Should check if there's four connect methods", () => {
+        cy.get('@xBullTitle').should('contain.text', 'xBull');
+        cy.get('@freighterTitle').should('contain.text', 'Freighter');
+        cy.get('@albedoTitle').should('contain.text', 'Albedo');
+        cy.get('@privateKeyTitle').should('contain.text', 'Private Key');
+    });
 
-        it('Should show the private key connect method', () => {
-            cy.get('@privateKeyBtn').click().as('privateKeyBtn');
-            cy.get('@container').should('contain.text', 'Connect with private key');
-        });
+    it('Should show the private key connect method', () => {
+        cy.get('@privateKeyBtn').click().as('privateKeyBtn');
+        cy.get('@container').should('contain.text', 'Connect with private key');
+    });
 
-        it('Should return to the four connect methods', () => {
-            cy.get('@privateKeyBtn').click();
-            cy.get('@container').should('contain.text', 'Connect with private key');
-            cy.get('.return-btn').click();
-            cy.get('@xBullTitle').should('contain.text', 'xBull');
-            cy.get('@freighterTitle').should('contain.text', 'Freighter');
-            cy.get('@albedoTitle').should('contain.text', 'Albedo');
-            cy.get('@privateKeyTitle').should('contain.text', 'Private Key');
-        });
+    it('Should return to the four connect methods', () => {
+        cy.get('@privateKeyBtn').click();
+        cy.get('@container').should('contain.text', 'Connect with private key');
+        cy.get('.return-btn').click();
+        cy.get('@xBullTitle').should('contain.text', 'xBull');
+        cy.get('@freighterTitle').should('contain.text', 'Freighter');
+        cy.get('@albedoTitle').should('contain.text', 'Albedo');
+        cy.get('@privateKeyTitle').should('contain.text', 'Private Key');
     });
 });
