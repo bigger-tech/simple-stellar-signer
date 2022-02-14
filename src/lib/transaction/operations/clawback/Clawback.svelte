44627dc71f4484df36f8a23158d74aeb088ad887
RivarolaNicolas <RivarolaNicolas94@gmail.com>
2022-02-14 11:45:53 -0300

    Add "Clawback" operation

---
+++ src/lib/transaction/operations/clawback/Clawback.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let optionalSource: string | undefined;
+    export let defaultSource: string;
+    export let asset: string;
+    export let amount: string;
+    export let from: string;
+</script>
+
+<div class="simple-signer clawback-operation">
+    <h3>Operation: Clawback</h3>
+
+    <p>Source account: {optionalSource ? optionalSource : defaultSource}</p>
+    <p>Asset: {asset}</p>
+    <p>Amount: {amount}</p>
+    <p>From: {from}</p>
+</div>
