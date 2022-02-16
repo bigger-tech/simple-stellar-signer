7a8ffec70f132963d1509091b1e975b1ccd3bd2d
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-02-16 11:53:41 -0300

    Delete sign helper, and refactor test

--- src/routes/sign/signHelper.ts
+++
@@ -1,9 +0,0 @@
-import type { Keypair, Transaction } from 'stellar-sdk';
-import { sendMessage } from '../../helpers/sendMessageHelpers';
-
-export function signTx(tx: Transaction, secretKey: Keypair) {
-    const signTx = tx.sign(secretKey);
-    const txSigned = tx.toXDR();
-    sendMessage(txSigned);
-    return signTx;
-}
