b981734fb5a262b8ed95d683082d962368acd2c3
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-02-25 12:12:38 -0300

    Refactor send message helpers

--- src/helpers/sendMessageHelpers.ts
+++ src/helpers/sendMessageHelpers.ts
@@ -1,13 +1,12 @@
-export function sendMessage(message: { ready?: boolean; message?: string }): void {
+import type { IReadyEvent } from './eventInterfaces/IReadyEvent';
+import type { IConnectedEvent } from './eventInterfaces/IConnectedEvent';
+import type { ISignedEvent } from './eventInterfaces/ISignedEvent';
+
+export function sendMessage(message: IConnectedEvent | IReadyEvent | ISignedEvent): void {
     const parentWindow = window.opener;
     parentWindow.postMessage(message, '*');
 }
 
-function closeWindowHandler() {
+export function closeWindow() {
     return window.close();
-}
-
-export default function messageHandler(message: string) {
-    sendMessage({ message });
-    closeWindowHandler();
 }
