116740c9aaf86fa550eef8ad88b062fd6e49a3d8
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-03-21 10:47:16 -0300

    Refactor group components function

--- src/lib/transaction/transactionGroupHelper.ts
+++ src/lib/transaction/transactionGroupHelper.ts
@@ -5,36 +5,42 @@ export default function groupComponents(
     operations: typeof OperationComponentTypes[],
     groups: IGroupsFromParam[],
 ): (typeof OperationComponentTypes | ITransactionGroup)[] {
-    const lastTo = groups[groups.length - 1]?.to;
+    const lastTo = groups[groups.length - 1];
     const group: (typeof OperationComponentTypes | ITransactionGroup)[] = [];
 
     if (groups.length === 0) {
         console.log("A group of operations wasn't provided");
         return operations;
-    } else if (!operations[lastTo!]) {
+    } else if (lastTo && operations[lastTo.to]) {
         console.error('There are fewer operations than the groups says');
     } else {
         let startIndex = 0;
 
         for (let i = 0; i < groups.length; i++) {
-            if (groups[i + 1] && groups[i]!.from > groups[i + 1]!.from) {
+            const currentGroup = groups[i];
+            const nextGroup = groups[i + 1];
+
+            if (nextGroup && currentGroup && currentGroup.from > nextGroup.from) {
                 console.error('The group object is not well sorted');
                 return operations;
             }
 
             const array: typeof OperationComponentTypes[] = [];
-            for (let k = startIndex; k < operations.length; k++) {
-                if (k >= groups[i]!.from && k < groups[i]!.to) {
-                    array.push(operations[k]!);
-                } else if (k === groups[i]!.to) {
-                    array.push(operations[k]!);
-                    group.push({ description: groups[i]!.description, operationsComponents: array });
-                    startIndex = k + 1;
-                    if (i != groups.length - 1) {
-                        break;
+            for (let j = startIndex; j < operations.length; j++) {
+                const currentOperation = operations[j];
+                if (currentGroup && currentOperation) {
+                    if (j >= currentGroup.from && j < currentGroup.to) {
+                        array.push(currentOperation);
+                    } else if (j === currentGroup.to) {
+                        array.push(currentOperation);
+                        group.push({ description: currentGroup.description, operationsComponents: array });
+                        startIndex = j + 1;
+                        if (i != groups.length - 1) {
+                            break;
+                        }
+                    } else {
+                        group.push(currentOperation);
                     }
-                } else {
-                    group.push(operations[k]!);
                 }
             }
         }
