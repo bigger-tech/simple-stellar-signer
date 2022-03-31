366dadc690cffa86239614c53c36b375d8fb85d1
Genebson <mauryavero@gmail.com>
2022-03-31 13:24:06 -0300

    Fix top-level await

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,13 @@
     import { onMount } from 'svelte';
 
     import { Router, Route } from 'svelte-navigator';
+    import WalletLanguage from './helpers/WalletLanguage';
     import Connect from './routes/connect/Connect.svelte';
     import Home from './routes/Home.svelte';
     import Sign from './routes/sign/Sign.svelte';
     import { detectLanguage, language } from './store/store';
     onMount(async () => {
-        return $language;
+        return ($language = await new WalletLanguage().getText());
     });
 </script>
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,5 +2,5 @@ import WalletLanguage from '../helpers/WalletLanguage';
 import { writable } from 'svelte/store';
 
 export const publicKey = writable('');
-export const language = writable(await new WalletLanguage().getText());
+export const language = writable(new WalletLanguage().getText());
 export const detectLanguage = writable(new WalletLanguage().getLanguage());
