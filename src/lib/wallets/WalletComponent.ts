522d518222c4677f2d6ed5fb2ccad13b0798cb90
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-03-31 19:26:31 -0300

    Use wallet class name property

--- src/lib/wallets/WalletComponent.ts
+++ src/lib/wallets/WalletComponent.ts
@@ -1,9 +1,14 @@
 import UnknownWalletError from '../errors/UnknownWalletError';
-import Albedo from './components/Albedo.svelte';
-import Freighter from './components/Freighter.svelte';
-import PrivateKey from './components/PrivateKey.svelte';
-import Rabet from './components/Rabet.svelte';
-import XBull from './components/XBull.svelte';
+import AlbedoComponent from './components/Albedo.svelte';
+import FreighterComponent from './components/Freighter.svelte';
+import PrivateKeyComponent from './components/PrivateKey.svelte';
+import RabetComponent from './components/Rabet.svelte';
+import XBullComponent from './components/XBull.svelte';
+import Albedo from '../../routes/connect/ui/wallets/Albedo';
+import Freighter from '../../routes/connect/ui/wallets/Freighter';
+import PrivateKey from '../../routes/connect/ui/wallets/PrivateKey';
+import Rabet from '../../routes/connect/ui/wallets/Rabet';
+import XBull from '../../routes/connect/ui/wallets/XBull';
 
 export default class WalletComponent {
     wallet: string;
@@ -12,20 +17,20 @@ export default class WalletComponent {
         let walletComponent: any;
 
         switch (wallet) {
-            case 'xbull':
-                walletComponent = XBull;
+            case XBull.NAME:
+                walletComponent = XBullComponent;
                 break;
-            case 'albedo':
-                walletComponent = Albedo;
+            case Albedo.NAME:
+                walletComponent = AlbedoComponent;
                 break;
-            case 'freighter':
-                walletComponent = Freighter;
+            case Freighter.NAME:
+                walletComponent = FreighterComponent;
                 break;
-            case 'rabet':
-                walletComponent = Rabet;
+            case Rabet.NAME:
+                walletComponent = RabetComponent;
                 break;
-            case 'privateKey':
-                walletComponent = PrivateKey;
+            case PrivateKey.NAME:
+                walletComponent = PrivateKeyComponent;
                 break;
             default:
                 undefined;
