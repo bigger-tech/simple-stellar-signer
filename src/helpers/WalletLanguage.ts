099510aa4ebab2c6fbdb8b9ba06f00455bb0fefd
Genebson <mauryavero@gmail.com>
2022-03-31 12:15:42 -0300

    Add language feature

--- src/helpers/WalletLanguage.ts
+++ src/helpers/WalletLanguage.ts
@@ -1,19 +1,14 @@
-import * as english from './languages/english.json';
-import * as spanish from './languages/spanish.json';
-
+import WalletLanguageFactory from './WalletLanguageFactory';
 export default class WalletLanguage {
-    getLanguage() {
-        const lang: any = ((navigator.languages && navigator.languages[0] && navigator.language) || 'en').substr(0, 2);
+    getLanguage(): string {
+        const lang: string = ((navigator.languages && navigator.languages[0] && navigator.language) || 'en').substr(
+            0,
+            2,
+        );
         return lang;
     }
-    getText() {
-        const moduleLanguage: any = { es: spanish, en: english };
-        const language = this.getLanguage();
-
-        if (moduleLanguage[language]) {
-            return moduleLanguage[language];
-        } else {
-            return moduleLanguage['en'];
-        }
+    getText(): any {
+        const language: string = this.getLanguage();
+        return new WalletLanguageFactory().create(language);
     }
 }
