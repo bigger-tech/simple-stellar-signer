0da2aa25d008979c97a68640c515adf16b8e16a0
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-04-04 12:00:17 -0300

    Add wallet factory test

---
+++ src/lib/wallets/__tests__/WalletFactory.test.ts
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expect } from '@jest/globals';
+import Albedo from '../albedo/Albedo';
+import Freighter from '../freighter/Freighter';
+import PrivateKey from '../privateKey/PrivateKey';
+import Rabet from '../rabet/Rabet';
+import XBull from '../xBull/XBull';
+import WalletFactory from '../WalletFactory';
+import InvalidWalletError from '../InvalidWalletError';
+const factory = new WalletFactory();
+
+it('should return a wallet', () => {
+    const albedo = factory.create('albedo');
+    const freighter = factory.create('freighter');
+    const privateKey = factory.create('privateKey');
+    const rabet = factory.create('rabet');
+    const xbull = factory.create('xbull');
+    expect(albedo).toBeInstanceOf(Albedo);
+    expect(freighter).toBeInstanceOf(Freighter);
+    expect(privateKey).toBeInstanceOf(PrivateKey);
+    expect(rabet).toBeInstanceOf(Rabet);
+    expect(xbull).toBeInstanceOf(XBull);
+});
+
+it('should throw an InvalidWalletError', () => {
+    expect(() => factory.create('albed')).toThrow(InvalidWalletError);
+});
+
+it('should return all wallets', () => {
+    const wallets = factory.createAll();
+    expect(wallets).toHaveLength(5);
+});
