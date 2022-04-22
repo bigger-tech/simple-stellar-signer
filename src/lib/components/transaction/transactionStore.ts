af2caca238883cf7ef2a92e01e5656315d7a0c09
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-04-22 15:04:40 -0300

    Delete store

--- src/lib/components/transaction/transactionStore.ts
+++ src/lib/components/transaction/transactionStore.ts
@@ -2,4 +2,3 @@ import { writable } from 'svelte/store';
 
 export const areOperationsExpanded = writable(false);
 export const operationsVisibility = writable([] as boolean[]);
-export const operationsHeight = writable([] as number[]);
