6001a2e704cd9af1ce1c111f2247be944cd2d0a6
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-05-04 20:11:20 -0300

    Fix sign

--- src/routes/sign/Sign.svelte
+++ src/routes/sign/Sign.svelte
@@ -2,7 +2,7 @@
     import Bridge from '../../lib/bridge/Bridge';
     import Transaction from '../../lib/components/transaction/Transaction.svelte';
     import { language } from '../../store/global';
-    import { isTransactionVisible, transaction } from './signStore';
+    import { transaction } from './signStore';
 
     const bridge = new Bridge();
     const urlParams = bridge.getTransactionMessageFromUrl();
@@ -10,22 +10,16 @@
     if (urlParams) {
         $transaction = urlParams;
     } else {
-        $isTransactionVisible = false;
         bridge.addTransactionMessageHandler((message) => {
             $transaction = message;
-            $isTransactionVisible = true;
         });
     }
 
     bridge.sendOnReadyEvent();
 </script>
 
-{#if $isTransactionVisible}
-    {#if $transaction?.xdr}
-        <Transaction transactionMessage={$transaction} />
-    {:else if !$transaction?.xdr}
-        <h1>{$language.XDR_NOT_PROVIDED}</h1>
-    {:else}
-        <p>{$language.LOADING}</p>
-    {/if}
+{#if $transaction?.xdr}
+    <Transaction transactionMessage={$transaction} />
+{:else if !$transaction.xdr}
+    <h1>{$language.XDR_NOT_PROVIDED}</h1>
 {/if}
