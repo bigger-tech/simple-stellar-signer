5ebe3314bbcc545876381eb088c018acaa62fa88
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-05-04 20:24:49 -0300

    Fix connect & sign window.open url and postMessage (#125)

--- src/routes/sign/Sign.svelte
+++ src/routes/sign/Sign.svelte
@@ -2,34 +2,24 @@
     import Bridge from '../../lib/bridge/Bridge';
     import Transaction from '../../lib/components/transaction/Transaction.svelte';
     import { language } from '../../store/global';
-    import { isTransactionVisible, transaction } from './signStore';
+    import { transaction } from './signStore';
 
-    const parent = window.opener;
     const bridge = new Bridge();
     const urlParams = bridge.getTransactionMessageFromUrl();
 
-    if (parent) {
-        $isTransactionVisible = false;
-    }
-
     if (urlParams) {
         $transaction = urlParams;
+    } else {
+        bridge.addTransactionMessageHandler((message) => {
+            $transaction = message;
+        });
     }
-
-    bridge.addTransactionMessageHandler((message) => {
-        $transaction = message;
-        $isTransactionVisible = true;
-    });
 
     bridge.sendOnReadyEvent();
 </script>
 
-{#if $isTransactionVisible}
-    {#if $transaction?.xdr}
-        <Transaction transactionMessage={$transaction} />
-    {:else if !$transaction?.xdr}
-        <h1>{$language.XDR_NOT_PROVIDED}</h1>
-    {:else}
-        <p>{$language.LOADING}</p>
-    {/if}
+{#if $transaction?.xdr}
+    <Transaction transactionMessage={$transaction} />
+{:else if !$transaction.xdr}
+    <h1>{$language.XDR_NOT_PROVIDED}</h1>
 {/if}
