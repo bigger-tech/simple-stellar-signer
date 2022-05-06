fa72b70487d41923e5659e1e7a79d9557d071a54
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2022-05-06 22:01:37 +1000

    Add buffer polyfill

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,28 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), splitVendorChunkPlugin()],
-    resolve: {
-        alias: {
-            util$: 'node_modules/util',
-        },
-    },
+    plugins: [svelte()],
+    resolve: {},
     server: {
         port: 3001,
     },
     optimizeDeps: {
-        // See https://github.com/mefechoel/svelte-navigator#im-using-vite-why-am-i-getting-errors-with-svelte-navigator
         exclude: ['svelte-navigator'],
         esbuildOptions: {
             // Node.js global to browser globalThis
             define: {
                 global: 'globalThis',
             },
+            // Enable esbuild polyfill plugins
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true,
+                }),
+            ],
         },
     },
     build: {
