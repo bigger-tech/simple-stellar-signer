7b7d43f46f53e92ebf84483075eddcf23ebd5e32
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2022-05-06 18:47:22 +1000

    Add explicit util dependency

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte(), splitVendorChunkPlugin()],
-    resolve: {},
+    resolve: {
+        alias: {
+            util$: 'node_modules/util',
+        },
+    },
     server: {
         port: 3001,
     },
