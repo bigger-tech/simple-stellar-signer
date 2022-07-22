96e1a4e79cedf365e37fdeb93afe27c0a3cef2cf
Genebson <mauryavero@gmail.com>
2022-07-22 13:29:08 -0300

    Add new store

--- src/lib/components/transaction/transactionStore.ts
+++ src/lib/components/transaction/transactionStore.ts
@@ -1,4 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const areOperationsExpanded = writable(false);
+export const isUserPublicKeyClicked = writable(false);
+export const isSourceAccountClicked = writable(false);
 export const operationsVisibility = writable([] as boolean[]);
