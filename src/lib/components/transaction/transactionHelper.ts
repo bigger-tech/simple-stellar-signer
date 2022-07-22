9ddcb426420c22879eb3253c631ca0f54eab4a81
Genebson <mauryavero@gmail.com>
2022-07-22 13:30:35 -0300

    Add getShortedStellarKey function

--- src/lib/components/transaction/transactionHelper.ts
+++ src/lib/components/transaction/transactionHelper.ts
@@ -1,3 +1,7 @@
+export function getShortedStellarKey(string: string): string {
+    return string.slice(0, 4).concat('...').concat(string.substr(-4));
+}
+
 export function checkIfAllAreTrue(arr: boolean[]): boolean {
     return arr.every((element) => element === true);
 }
