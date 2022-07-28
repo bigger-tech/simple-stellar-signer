c3d7b931444f9c2ba10b71b59663cf82d3bc1f98
Genebson <mauryavero@gmail.com>
2022-07-28 17:00:58 -0300

    Add Header component

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { isLogoutVisible } from '../../../store/global';
+    import type IStorage from '../../storage/IStorage';
+    import LocalStorage from '../../storage/storage';
+    import clickOutside from '../helpers/clickOutside';
+    import Language from '../language/Language.svelte';
+    import { isLanguageMenuVisible } from '../language/languageStore';
+    import Logout from '../logout/Logout.svelte';
+
+    const storage: IStorage = new LocalStorage();
+    const isUserConnected = storage.getItem('wallet');
+
+    function hideMenu() {
+        $isLanguageMenuVisible = false;
+        $isLogoutVisible = false;
+    }
+</script>
+
+<header class="simple-signer header row-space-between-center header-border">
+    <div class="simple-signer logout-language-container row-space-between-center" use:clickOutside={hideMenu}>
+        <div class="simple-signer logout-container">
+            {#if isUserConnected}
+                <Logout storage={storage} />
+            {/if}
+        </div>
+        <div class="simple-signer language-container">
+            <div class="simple-signer column-center">
+                <Language />
+            </div>
+        </div>
+    </div>
+</header>
+
+<style>
+    .simple-signer.row-space-between-center {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin-left: 129px;
+    }
+
+    .simple-signer.column-center {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
