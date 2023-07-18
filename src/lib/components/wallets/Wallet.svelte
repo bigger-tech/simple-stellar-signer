792980dda05d16e959adb18089a95dc60d2ab933
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-07-18 10:41:03 -0300

    Add isInstalled prop for the Wallet component

--- src/lib/components/wallets/Wallet.svelte
+++ src/lib/components/wallets/Wallet.svelte
@@ -6,9 +6,9 @@
     import PrivateKey from '../../wallets/privateKey/PrivateKey';
 
     export let wallet: IWallet;
+    export let isInstalled: boolean;
 
     const dispatch = createEventDispatcher();
-    const isInstalled = wallet.isInstalled();
 
     async function connect(): Promise<void> {
         if (wallet.getName() === PrivateKey.NAME) {
@@ -20,22 +20,20 @@
 </script>
 
 <div class="simple-signer wallet-container">
-    {#await isInstalled then isInstalled}
-        <div class="simple-signer wallet-items-container {isInstalled ? 'shadow' : ''}" on:click={connect}>
-            <div class="simple-signer wallet-items {isInstalled ? '' : 'wallet-opacity'}">
-                <div id="wallet-icon" class="simple-signer wallet-logo">
-                    <svelte:component this={wallet.getSvgIcon()} />
-                </div>
-                <span class="simple-signer wallet-title {isInstalled ? '' : 'wallet-title-opacity'}">
-                    {wallet.getFriendlyName()}
-                </span>
+    <div class="simple-signer wallet-items-container {isInstalled ? 'shadow' : ''}" on:click={connect}>
+        <div class="simple-signer wallet-items {isInstalled ? '' : 'wallet-opacity'}">
+            <div id="wallet-icon" class="simple-signer wallet-logo">
+                <svelte:component this={wallet.getSvgIcon()} />
             </div>
-
-            <a class="simple-signer {isInstalled ? '' : 'install-wallet'}" target="_blank" href={wallet.getExtension()}>
-                {isInstalled ? '' : $language.INSTALL}
-            </a>
+            <span class="simple-signer wallet-title {isInstalled ? '' : 'wallet-title-opacity'}">
+                {wallet.getFriendlyName()}
+            </span>
         </div>
-    {/await}
+
+        <a class="simple-signer {isInstalled ? '' : 'install-wallet'}" target="_blank" href={wallet.getExtension()}>
+            {isInstalled ? '' : $language.INSTALL}
+        </a>
+    </div>
 </div>
 
 <style>
