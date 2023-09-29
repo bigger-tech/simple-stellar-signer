b26b014278407e5f42c439750640b0ed74198887
github_fedeesti <fede.estigarribia@hotmail.com>
2023-09-29 10:30:33 -0300

    Add StellarNetworks and fix imports in freighter.test

--- src/lib/wallets/freighter/__test__/freighter.test.ts
+++ src/lib/wallets/freighter/__test__/freighter.test.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { expect } from '@jest/globals';
+import { signTransaction } from '@stellar/freighter-api';
 import SorobanClient from 'soroban-client';
 
+import { StellarNetwork } from '../../../stellar/StellarNetwork';
 import LocalStorage from '../../../storage/storage';
 import Freighter from '../Freighter';
 
@@ -25,22 +27,21 @@ jest.mock('stellar-sdk', () => {
 describe('Freighter management', () => {
     const storage = new LocalStorage();
     let freighter: Freighter;
+    const freighterNetwork = StellarNetwork.FUTURENET.toUpperCase();
 
     beforeEach(() => {
         jest.clearAllMocks();
         freighter = new Freighter(storage);
     });
     it('Should sign a transaction successfully from the Futurenet network', async () => {
-        const { signTransaction } = require('@stellar/freighter-api');
         const signedXdr =
             'AAAAAgAAAAA2jYMwhev3yM7P+JWOv6kRQZAssek5zytAbbyhJbOjNQAAAGQAATOSAAAAAQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAA2jYMwhev3yM7P+JWOv6kRQZAssek5zytAbbyhJbOjNQAAAAAF9eEAAAAAAAAAAAA=';
-        const tx = SorobanClient.TransactionBuilder.fromXDR(signedXdr, SorobanClient.Networks.STANDALONE);
-
+        const tx = SorobanClient.TransactionBuilder.fromXDR(signedXdr, SorobanClient.Networks.FUTURENET);
         await freighter.sign(tx);
 
-        expect(freighter.freighterNetwork).toEqual('FUTURENET');
+        expect(freighter.freighterNetwork).toEqual(freighterNetwork);
         expect(signTransaction).toHaveBeenCalledWith(signedXdr, {
-            network: 'FUTURENET',
+            network: freighterNetwork,
         });
         expect(signTransaction).toHaveBeenCalledTimes(1);
     });
