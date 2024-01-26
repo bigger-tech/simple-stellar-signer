ebff1a28192e0da153ff3e787e2292e46ad4b45e
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2024-01-26 15:41:25 -0300

    Add isFeeSourceAccountClicked store variable

--- src/lib/components/transaction/transactionStore.ts
+++ src/lib/components/transaction/transactionStore.ts
@@ -3,4 +3,5 @@ import { writable } from 'svelte/store';
 export const areOperationsExpanded = writable(false);
 export const isUserPublicKeyClicked = writable(false);
 export const isSourceAccountClicked = writable(false);
+export const isFeeSourceAccountClicked = writable(false);
 export const operationsVisibility = writable([] as boolean[]);
