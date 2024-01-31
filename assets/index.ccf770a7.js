ae005e5959de75bfd2bc1ee107479cb276358d14
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2024-01-31 14:59:05 +0000

    Deploying to gh-pages from @ ScaleMote/simple-stellar-signer@3ab98ab3edb6d30f287c9943dca174d2c90e9fac 🚀

--- assets/index.ccf770a7.js
+++ assets/index.ccf770a7.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Uo,y as gt,p as q}from"./index.b664e5ae.js";/**
+import{n as _t,s as ce,T as N,t as U,a as E,o as ot,R as Uo,y as gt,p as q}from"./index.c0e2038c.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
