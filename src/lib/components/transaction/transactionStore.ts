424eec216111ac44a54f4e13b9511573b200e474
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2024-01-31 11:36:33 -0300

    Delete isFeeSourceAccountClicked from transactionStore

--- src/lib/components/transaction/transactionStore.ts
+++ src/lib/components/transaction/transactionStore.ts
@@ -3,5 +3,4 @@ import { writable } from 'svelte/store';
 export const areOperationsExpanded = writable(false);
 export const isUserPublicKeyClicked = writable(false);
 export const isSourceAccountClicked = writable(false);
-export const isFeeSourceAccountClicked = writable(false);
 export const operationsVisibility = writable([] as boolean[]);
