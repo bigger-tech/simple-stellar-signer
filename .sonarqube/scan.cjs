be7507f7f7181a6e92259b64a6e596ef9d2f2ba0
Juan Avero <64806950+jjuannn@users.noreply.github.com>
2024-04-15 12:40:24 -0300

    Setup files for CodeBuild (mainnet) (#204)
    * Create sonarqube and parse output scripts for CodeBuild
    
    * Create buildspec.yml file for CodeBuild
    
    * Replace sonarqube scan script file extension from .js to .cjs
    
    * Skip qualityCheck in sonar script to avoid problems with Svelte
    
    * Remove 'token' property in sonar scanner config since it's not required for sonarqube v9

---
+++ .sonarqube/scan.cjs
@@ -0,0 +1,25 @@
+const scanner = require('sonarqube-scanner');
+const argv = require('minimist')(process.argv.slice(2));
+
+const { token, host, project } = argv;
+
+if (!host || !token || !project) {
+    throw new Error('Too few arguments');
+}
+
+scanner(
+    {
+        serverUrl: host,
+        login: token,
+        options: {
+            'sonar.projectName': project,
+            'sonar.exclusions':
+                '**/main.ts, **/configuration/*.ts, **/*.module.ts, **/*.interface.ts, **/*.domain.ts, **/*.enum.ts, **/*.dto.ts, **/*.spec.ts, **/*.client.ts, **/*.errors.ts, **/*.schema.ts, auth.dynamo.utils.ts',
+            'sonar.sources': 'src',
+            'sonar.language': 'ts',
+            'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+            'sonar.sourceEncoding': 'UTF-8',
+        },
+    },
+    () => process.exit(),
+);
