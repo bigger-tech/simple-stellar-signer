998b256a283ac360fe5d7f1f28eb50d8db3cbba6
CERP27 <carloscer27r@gmail.com>
2024-05-10 18:34:11 -0300

    Create InvokeHostFunctionComponentFactory:
    Create InvokeHostFunctionComponentFactory to create the invokeHostFunctionComponent with specific paramentes only avaliable using asynchronus code

---
+++ src/lib/components/transaction/operations/InvokeHostFunctionOperationComponentFactory.ts
@@ -0,0 +1,27 @@
+import type { Operation, Transaction } from 'stellar-sdk';
+
+import { getContractAddress, getContractMethodsParams } from '../../../soroban/GetContractFunctionInfo';
+import InvokeHostFunctionComponent from './invokeHostFunction/InvokeHostFunctionComponent';
+
+export class InvokeHostFunctionComponentFactory {
+    async create(tx: Transaction, operation: Operation): Promise<InvokeHostFunctionComponent> {
+        const funcTitle = (operation as Operation.InvokeHostFunction).func.invokeContract().functionName().toString();
+        const contractID = getContractAddress(
+            (operation as Operation.InvokeHostFunction).func
+                .invokeContract()
+                .contractAddress()
+                .contractId()
+                .toString('hex'),
+        );
+        const funcParameters = await getContractMethodsParams(contractID, funcTitle);
+
+        const operationComponent = new InvokeHostFunctionComponent(
+            tx,
+            operation as Operation.InvokeHostFunction,
+            funcParameters[0]!,
+            funcTitle,
+        );
+
+        return operationComponent;
+    }
+}
