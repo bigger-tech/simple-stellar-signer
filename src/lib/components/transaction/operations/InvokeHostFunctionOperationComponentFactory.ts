60421b14160cc8a953113131545e277247e597f2
CERP27 <carloscer27r@gmail.com>
2024-05-20 10:39:29 -0300

    Update InvokeHostFunctionComponentFactory:
    - Add funcType parameter to know wich type of InvokeHostFunction is being called
    - Add a condition to return the InvokeHostFunctionComponent with all the info when the type is invokeContract
    - Return InvokeHostFunctionComponent with just the type if the type is not invokeContract
    - Refactor the code

--- src/lib/components/transaction/operations/InvokeHostFunctionOperationComponentFactory.ts
+++ src/lib/components/transaction/operations/InvokeHostFunctionOperationComponentFactory.ts
@@ -1,28 +1,34 @@
 import type { Operation, Transaction } from 'stellar-sdk';
 
 import { getContractAddress, getContractMethodsParams } from '../../../soroban/GetContractFunctionInfo';
+import { InvokeHostFunction, InvokeHostFunctionType } from '../../../stellar/InvokeHostFunction';
 import InvokeHostFunctionComponent from './invokeHostFunction/InvokeHostFunctionComponent';
 
 export class InvokeHostFunctionComponentFactory {
     async create(tx: Transaction, operation: Operation): Promise<InvokeHostFunctionComponent> {
-        const funcTitle = (operation as Operation.InvokeHostFunction).func.invokeContract().functionName().toString();
-        const contractID = getContractAddress(
-            (operation as Operation.InvokeHostFunction).func
-                .invokeContract()
-                .contractAddress()
-                .contractId()
-                .toString('hex'),
-        );
-        const funcParameters = await getContractMethodsParams(contractID, funcTitle);
+        const invokeHostFunction = operation as Operation.InvokeHostFunction;
+        const funcType = invokeHostFunction.func.switch().name;
 
-        const operationComponent = new InvokeHostFunctionComponent(
+        if (funcType === InvokeHostFunctionType.InvokeContract) {
+            const funcTitle = invokeHostFunction.func.invokeContract().functionName().toString();
+
+            const contractID = getContractAddress(
+                invokeHostFunction.func.invokeContract().contractAddress().contractId().toString('hex'),
+            );
+            const funcParameters = await getContractMethodsParams(contractID, funcTitle);
+
+            return new InvokeHostFunctionComponent(tx, invokeHostFunction, contractID, funcTitle, funcParameters[0]!);
+        }
+
+        return new InvokeHostFunctionComponent(
             tx,
-            operation as Operation.InvokeHostFunction,
-            contractID,
-            funcTitle,
-            funcParameters[0]!,
+            invokeHostFunction,
+            undefined,
+            undefined,
+            undefined,
+            funcType === InvokeHostFunctionType.CreateContract
+                ? InvokeHostFunction.CreateContract
+                : InvokeHostFunction.UploadWasm,
         );
-
-        return operationComponent;
     }
 }
