2c46776055363d4780913782f039f39ccb369723
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-09-20 17:14:04 -0300

    Add Google Analytics in head of project

---
+++ src/lib/components/analytics/GoogleAnalytics.svelte
@@ -0,0 +1,39 @@
+<script lang="ts" context="module">
+    declare let gtag: (...args: any[]) => void;
+</script>
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    import { GOOGLE_ANALYTICS_ID } from '../../../constants';
+
+    onMount(() => {
+        const script = document.createElement('script');
+        script.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', '${GOOGLE_ANALYTICS_ID}');
+        `;
+
+        document.head.appendChild(script);
+    });
+
+    $: {
+        if (typeof gtag !== 'undefined') {
+            gtag('config', GOOGLE_ANALYTICS_ID, {
+                page_title: document.title,
+                page_path: window.location.pathname,
+            });
+        }
+    }
+</script>
+
+<svelte:head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id={GOOGLE_ANALYTICS_ID}">
+    </script>
+</svelte:head>
