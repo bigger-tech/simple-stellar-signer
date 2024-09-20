0e9fbe5b9b94cd71ce3201e63e94b01eac6246c7
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-09-20 13:38:53 -0300

    Add spinner while logging out

--- src/routes/logout/Logout.svelte
+++ src/routes/logout/Logout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import { afterUpdate } from 'svelte';
+    import { BarLoader } from 'svelte-loading-spinners';
 
     import Bridge, { SimpleSignerPageType } from '../../lib/bridge/Bridge';
     import type { WalletConnectService } from '../../lib/service/walletConnect';
     import type IStorage from '../../lib/storage/IStorage';
     import LocalStorage from '../../lib/storage/storage';
+    import { language } from '../../store/global';
 
     export let walletConnectService: WalletConnectService;
     export let isServiceInitialized: boolean;
@@ -20,3 +22,17 @@
         }
     });
 </script>
+
+<div class="simple-signer wallets-loading-container">
+    <p>{$language.LOGGING_OUT}</p>
+    <BarLoader color="#2f69b7" size={40} />
+</div>
+
+<style>
+    .wallets-loading-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: 'Roboto', sans-serif;
+    }
+</style>
